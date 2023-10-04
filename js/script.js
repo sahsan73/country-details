4f24e9595aa0017bb7070955d09f8eefdf081f36
Saquib Ahsan <saquibahsan73@gmail.com>
2023-10-04 11:43:46 +0530

    implement UI get details btns

---
+++ js/script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const getDetailsContainer = document.querySelector(".get-details");
+const btnCountryDetails = document.querySelector(".btn-country-details");
+const btnWhere = document.querySelector(".btn-where");
