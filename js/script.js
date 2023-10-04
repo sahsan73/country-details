9aa109f971803844b550c17f10af13ba101ba9a8
Saquib Ahsan <saquibahsan73@gmail.com>
2023-10-04 12:05:38 +0530

    country-details-card

--- js/script.js
+++ js/script.js
@@ -3,3 +3,7 @@
 const getDetailsContainer = document.querySelector(".get-details");
 const btnCountryDetails = document.querySelector(".btn-country-details");
 const btnWhere = document.querySelector(".btn-where");
+
+btnCountryDetails.addEventListener("click", function () {
+  getDetailsContainer.classList.add("hidden");
+});
